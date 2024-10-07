8c4bce77d7eefa646c8e75e356ade30270119bf2
Daniel Johnson <Daniel.Johnson@greensboro-nc.gov>
2024-10-07 08:12:54 -0400

    feat: add more db entities

---
+++ GutCheck.Core/Entities/FoodRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace GutCheck.Core.Entities
+{
+    public class FoodRecord
+    {
+        public int FoodRecordId { get; set; }
+        public int UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public UInt16 HealthScore { get; set; }
+        public UInt16 PortionSize { get; set; }
+        
+    }
+}
---
+++ GutCheck.Core/Entities/WeightRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace GutCheck.Core.Entities
+{
+    public class WeightRecord
+    {
+        public int WeightRecordId { get; set; }
+        public int UserId { get; set; }
+        public decimal Weight { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
