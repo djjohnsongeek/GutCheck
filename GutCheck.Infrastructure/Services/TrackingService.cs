a8e31b362018d29acdd1da4a4306f712957ea3d7
Daniel Johnson <Daniel.Johnson@greensboro-nc.gov>
2024-10-07 08:14:02 -0400

    feat: add new service for tracking bus logic

---
+++ GutCheck.Infrastructure/Services/TrackingService.cs
@@ -0,0 +1,20 @@
+﻿using GutCheck.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GutCheck.Infrastructure.Services
+{
+    public class TrackingService
+    {
+        private IWeightRecordRepository _weightRepo;
+
+        public TrackingService(IWeightRecordRepository weightRecordRepository)
+        {
+            _weightRepo = weightRecordRepository;
+        }
+
+    }
+}
